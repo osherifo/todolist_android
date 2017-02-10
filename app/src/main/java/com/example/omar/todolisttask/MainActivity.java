5c8d765d49d6bb6baa394736ea6c20f9b56f5443
Omar Sherif <osherifo93@gmail.com>
2017-02-10 17:36:26 +0200

    Initial layout and firebase configurations

--- app/src/main/java/com/example/omar/todolisttask/MainActivity.java
+++ app/src/main/java/com/example/omar/todolisttask/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.omar.todolisttask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+
+    public void addItemButtonListener(View view){
+        EditText todoTextView = (EditText) findViewById(R.id.add_todo_text);
+        String todo= todoTextView.getText().toString();
+        if(!todo.isEmpty()) {
+            Log.i("TODO:", todo);
+            todoTextView.setText("");
+        }
+
     }
 }
